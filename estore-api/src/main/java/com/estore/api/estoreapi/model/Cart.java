0201800861525e051608d45a1b881975b925a33c
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-02-28 01:12:26 -0500

    Added logs to Cart.java

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Cart.java
@@ -14,12 +14,23 @@ public class Cart {
     private Map<Integer, Integer> itemsMap;
     private int userId;
 
+    private InventoryDAO inventoryDao;
+
     /**
      * Creates a cart for a given user
      * @param userId The id of the user who owns the cart
      */
     public Cart(int userId) {
         itemsMap = new HashMap<>();
+        this.userId = userId;
+    }
+
+    /**
+     * Sets the inventoryDao for the cart
+     * @param inventoryDao The inventoryDao for the cart
+     */
+    public void setInventoryDao(InventoryDAO inventoryDao) {
+        this.inventoryDao = inventoryDao;
     }
 
     /**
@@ -53,35 +64,59 @@ public class Cart {
      * Adds a product to the cart
      * @param sku The sku of the product to add
      * @param quantity The quantity of the product to add
+     * 
+     * @return true if the product was added to the cart, false otherwise
      */
-    public void addProduct(int sku, int quantity) {
+    public boolean addProduct(int sku, int quantity) {
+        LOG.info("Adding product with sku " + sku + " and quantity " + quantity + " to cart for user " + userId);
         int currentQuantity = itemsMap.getOrDefault(sku, 0);
-        itemsMap.put(sku, currentQuantity + quantity);
+        int newQuantity = currentQuantity + quantity;
+
+        // check the inventory to see if the product is available at the new quantity
+        try {
+            if (inventoryDao.getProduct(sku).hasEnoughStockFor(newQuantity)) {
+                itemsMap.put(sku, newQuantity);
+                LOG.info("Product with sku " + sku + " added to cart for user " + userId);
+                return true;
+            }
+        } catch (IOException e) {
+            LOG.severe("Error getting inventory for sku " + sku);
+            return false;
+        }
+
+        return false;
     }
 
     /**
      * Removes a product from the cart
      * @param sku The sku of the product to remove
      * @param quantity The quantity of the product to remove
+     * 
+     * @return true if the product was removed from the cart, false otherwise
      */
-    public void removeProduct(int sku, int quantity) {
-        int currentQuantity = itemsMap.getOrDefault(sku, 0);
+    public boolean removeProduct(int sku, int quantity) {
+        LOG.info("Removing product with sku " + sku + " and quantity " + quantity + " from cart for user " + userId);
 
-        // If the quantity is somehow zero or negative, remove the product from the cart
-        if (currentQuantity <= 0) {
-            // remove the product from the cart
-            itemsMap.remove(sku);
-            return;
+        // If the product is not in the cart, return false
+        if (!itemsMap.containsKey(sku)) {
+            LOG.warning("Product with sku " + sku + " is not in the cart for user " + userId);
+            return false;
         }
+
+        int currentQuantity = itemsMap.get(sku);
 
         // If the quantity to remove is greater than the quantity in the cart,
         // remove the product from the cart
         int newQuantity = currentQuantity - quantity;
         if (newQuantity <= 0) {
             itemsMap.remove(sku);
+            LOG.info("Product with sku " + sku + " removed from cart for user " + userId);
         } else {
             itemsMap.put(sku, newQuantity);
+            LOG.info("Product with sku " + sku + " quantity reduced to " + newQuantity + " in cart for user " + userId);
         }
+
+        return true;
     }
 
     /**
@@ -89,22 +124,24 @@ public class Cart {
      * @param sku The sku of the product to remove
      * @param quantity The quantity of the product to remove
      */
-    public void clear() { itemsMap.clear(); }
+    public void clear() { 
+        itemsMap.clear();
+        LOG.info("Cart for user " + userId + " cleared");
+    }
 
     /**
      * Calculates the total price of the cart
-     * @param inventory The inventory to use to calculate the total
      * @return The total price of the cart
      * @throws IOException If there is an error reading from the inventory
      */
-    public float getTotalPrice(InventoryDAO inventory) throws IOException {
+    public float getTotalPrice() throws IOException {
         float total = 0;
 
         // Iterate through the products in the cart and calculate the total
         for (Map.Entry<Integer, Integer> entry : itemsMap.entrySet()) {
             int sku = entry.getKey();
             int quantity = entry.getValue();
-            Product product = inventory.getProduct(sku);
+            Product product = inventoryDao.getProduct(sku);
 
             // If the product is not in the inventory, it is not added to the total
             if (product != null) {
@@ -112,6 +149,7 @@ public class Cart {
             }
         }
 
+        LOG.info("Total price for cart for user " + userId + " is " + total);
         return total;
     }
 }
