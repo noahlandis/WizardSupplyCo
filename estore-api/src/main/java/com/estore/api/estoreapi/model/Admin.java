77b9a541ba114dc01458fcffa102975ceddcd874
Priyank Patel <pnp2147@rit.edu>
2023-03-04 13:48:12 -0500

    Added Javadoc for Admin class

--- estore-api/src/main/java/com/estore/api/estoreapi/model/Admin.java
+++ estore-api/src/main/java/com/estore/api/estoreapi/model/Admin.java
@@ -7,6 +7,11 @@ public class Admin extends User {
 
     private static final String ADMIN_USER_NAME = "admin";
     private static final int ADMIN_USER_ID = 1;
+    
+    /**
+     * Creates a user which has a userName and userId unique
+     * to the estore Owner 
+     */
     public Admin(){
         super(ADMIN_USER_ID,ADMIN_USER_NAME);
     }
