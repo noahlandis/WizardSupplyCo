bc6f749aa6087f7dd819662880cba570c032a780
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-03-14 16:33:23 -0400

    Created test components for route testing

---
+++ estore-ui/src/app/feature/test-catalog/test-catalog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-catalog',
+  templateUrl: './test-catalog.component.html',
+  styleUrls: ['./test-catalog.component.css']
+})
+export class TestCatalogComponent {
+
+}
