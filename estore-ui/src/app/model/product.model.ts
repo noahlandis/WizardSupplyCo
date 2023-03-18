b7f0558b0ac3a2a1955190f02031a00ea63aafc4
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-03-18 02:45:35 -0400

    Update product model to include base class w/o sku

--- estore-ui/src/app/model/product.model.ts
+++ estore-ui/src/app/model/product.model.ts
@@ -1,3 +1,22 @@
-export class Product {
-  constructor(public name: string, public sku: number, public description: string, public price: number) { }
+export class BaseProduct {
+  constructor(
+    public name: string,
+    public price: number,
+    public stockQuantity: number,
+    public images: string[],
+    public description: string
+  ) { }
+}
+
+export class Product extends BaseProduct {
+  constructor(
+    public sku: number,
+    name: string,
+    price: number,
+    stockQuantity: number,
+    images: string[],
+    description: string
+  ) {
+    super(name, price, stockQuantity, images, description);
+  }
 }
