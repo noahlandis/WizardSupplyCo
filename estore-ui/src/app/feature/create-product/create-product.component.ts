18193dec6a88638d234b825cf635c00f97e61a75
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-03-18 02:45:56 -0400

    Implement create product component

--- estore-ui/src/app/feature/create-product/create-product.component.ts
+++ estore-ui/src/app/feature/create-product/create-product.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { ProductService } from 'src/app/services/product.service';
+import { BaseProduct } from 'src/app/model/product.model';
 
 @Component({
   selector: 'app-create-product',
@@ -6,5 +11,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent {
+  editProductForm: FormGroup;
+  
+  @ViewChild('createButtonText', { static: false }) createButtonText!: ElementRef;
 
+  isLoading = false;
+  isSuccess = false;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private productService: ProductService,
+    ) {
+      this.editProductForm = this.fb.group({
+        name: ['', [Validators.required]],
+        // price is required, and must be a decimal number with 2 decimal places
+        price: ['', [Validators.required, Validators.pattern('[0-9]+(\.[0-9][0-9]?)?')]],
+        stockQuantity: ['', [Validators.required, Validators.pattern('[0-9]*')]],
+        // images is required, and must be a comma-separated list of URL strings
+        images: ['', [Validators.pattern('^(https?:\/\/.*\.(?:png|jpg|jpeg|gif|png|svg))(?:, https?:\/\/.*\.(?:png|jpg|jpeg|gif|png|svg))*$')]],
+        description: ['', []]
+      });
+  }
+
+  ngOnInit(): void { }
+
+  /** Create product form submittion handler */
+  async create(): Promise<void> {
+    if (!this.editProductForm.valid)
+      return;
+    
+    const { name, price, stockQuantity, images, description } = this.editProductForm.value;
+    const imagesArray: string[] = images.split(', ');
+
+    const product = new BaseProduct(name, price, stockQuantity, imagesArray, description);
+    console.log('Create product form submitted with product:', JSON.stringify(product));
+
+    // call update product on the product service
+    this.productService.addProduct(product).subscribe({
+      next: async (updateSuccess) => {
+        if (updateSuccess) {
+          console.log('Product updated successfully');
+        } else {
+          console.log('Product update failed');
+        }
+        this.showSubmitFeedback();
+      },
+      error: (e) => {
+        console.log('Product update failed with error:', e);
+      },
+    });
+  }
+
+  async showSubmitFeedback(): Promise<void> {
+    this.createButtonText.nativeElement.textContent = '';
+    this.isLoading = true;
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    this.isLoading = false;
+    this.isSuccess = true;
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    this.isSuccess = false;
+    this.createButtonText.nativeElement.textContent = 'Create';
+  }
 }
