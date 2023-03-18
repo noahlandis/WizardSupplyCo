e67e4456cfefee287179e748f865e0e106c06367
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-03-18 01:58:13 -0400

    created skeleton CreateProductComponent

--- estore-ui/src/app/feature/feature.module.ts
+++ estore-ui/src/app/feature/feature.module.ts
@@ -7,9 +7,10 @@ import { TestCartComponent } from './test-cart/test-cart.component';
 import { TestAdminDashboardComponent } from './test-admin-dashboard/test-admin-dashboard.component';
 import { TestCatalogComponent } from './test-catalog/test-catalog.component';
 import { LoginRegistrationComponent } from './login-registration/login-registration.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 @NgModule({
-    declarations: [TestCartComponent, TestAdminDashboardComponent, TestCatalogComponent, LoginRegistrationComponent],
+    declarations: [TestCartComponent, TestAdminDashboardComponent, TestCatalogComponent, LoginRegistrationComponent, CreateProductComponent],
     imports: [
         CommonModule,
         MaterialModule,
