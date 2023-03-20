133d0a76c3baed80e3a55802141350ac5ff3e457
Kanisha <kanisha.agrawal@axiomtelecom.com>
2023-03-19 20:44:25 -0400

    productCard

--- estore-ui/src/app/app-routing.module.ts
+++ estore-ui/src/app/app-routing.module.ts
@@ -11,44 +11,45 @@ import { LoginRegistrationComponent } from './feature/login-registration/login-r
 import { TestAdminDashboardComponent } from './feature/test-admin-dashboard/test-admin-dashboard.component';
 import { EditProductComponent } from './feature/edit-product/edit-product.component';
 import { CreateProductComponent } from './feature/create-product/create-product.component';
+import { CatalogProductCardComponent } from './feature/catalog-product-card/catalog-product-card.component';
 
 const routes: Routes = [
-    {path:'', component:TestCatalogComponent},
-    {path:'catalog', component:TestCatalogComponent},
-    {
-        path:'cart',
-        component:TestCartComponent,
-        canActivate: [authCustomerGuard]
-    },
-    {
-        path:'checkout',
-        component:TestCartComponent,
-        canActivate: [authCustomerGuard]
-    },
-    {
-        path:'auth',
-        component:LoginRegistrationComponent,
-        canActivate: [noAuthGuard]
-    },
-    {
-        path:'admin',
-        component:TestAdminDashboardComponent,
-        canActivate: [authAdminGuard]
-    },
-    {
-        path:'edit-product/:sku',
-        component:EditProductComponent,
-        canActivate: [authAdminGuard]
-    },
-    {
-        path:'create-product',
-        component:CreateProductComponent,
-        canActivate: [authAdminGuard]
-    }
+  { path: '', component: CatalogProductCardComponent },
+  { path: 'catalog', component: TestCatalogComponent },
+  {
+    path: 'cart',
+    component: TestCartComponent,
+    canActivate: [authCustomerGuard],
+  },
+  {
+    path: 'checkout',
+    component: TestCartComponent,
+    canActivate: [authCustomerGuard],
+  },
+  {
+    path: 'auth',
+    component: LoginRegistrationComponent,
+    canActivate: [noAuthGuard],
+  },
+  {
+    path: 'admin',
+    component: TestAdminDashboardComponent,
+    canActivate: [authAdminGuard],
+  },
+  {
+    path: 'edit-product/:sku',
+    component: EditProductComponent,
+    canActivate: [authAdminGuard],
+  },
+  {
+    path: 'create-product',
+    component: CreateProductComponent,
+    canActivate: [authAdminGuard],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- estore-ui/src/app/feature/catalog-product-card/catalog-product-card.component.ts
+++ estore-ui/src/app/feature/catalog-product-card/catalog-product-card.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-catalog-product-card',
   templateUrl: './catalog-product-card.component.html',
-  styleUrls: ['./catalog-product-card.component.css']
+  styleUrls: ['./catalog-product-card.component.css'],
 })
 export class CatalogProductCardComponent {
+  @Input() name = '';
+  @Input() price = '';
+  @Input() description = '';
+  @Input() image = '';
 
+  constructor() {}
 }
