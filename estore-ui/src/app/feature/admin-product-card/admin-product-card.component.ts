317979e1a80b6661e397d6447b1827780d34be0b
Noah Landis <nl7665@rit.edu>
2023-03-20 19:08:57 -0400

    product card skeleton

---
+++ estore-ui/src/app/feature/admin-product-card/admin-product-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-card',
+  templateUrl: './admin-product-card.component.html',
+  styleUrls: ['./admin-product-card.component.css']
+})
+export class AdminProductCardComponent {
+
+}
