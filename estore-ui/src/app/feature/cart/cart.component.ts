ee85bcc9bffcb6e58509980ad7b9b2d9da1e3f37
2nPlusOne <39078738+2nPlusOne@users.noreply.github.com>
2023-03-20 07:32:52 -0400

    Finished cart and search cart integration

--- estore-ui/src/app/feature/cart/cart.component.ts
+++ estore-ui/src/app/feature/cart/cart.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CartsService } from 'src/app/services/carts.service';
+import { UsersService } from 'src/app/services/users.service';
+import { Subscription } from 'rxjs';
+import { UpdateService } from 'src/app/services/update.service';
+import { Router } from '@angular/router';
+
+class CartMapEntry {
+  constructor(public sku: number, public quantity: number) {}
+}
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnInit, OnDestroy {
+  private cartUpdateSubscription?: Subscription;
+  
+  userId: number = 1;
+  cartEntries: CartMapEntry[] = [];
+  cartCount: number = 0;
+  totalPrice: number = 0;
 
+  constructor(private usersService: UsersService,
+    private cartsService: CartsService,
+    private updateService: UpdateService,
+    ) { }
+
+  ngOnInit(): void {
+    this.getUserId();
+    this.getCartEntries();
+
+    // create an infinite subscription to the cart update observable
+    this.cartUpdateSubscription = this.updateService.cartUpdate$.subscribe(() => {
+      this.getCartEntries(); // Fetch the cart contents when the cart is updated
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Unsubscribe from the cart update observable
+    if (this.cartUpdateSubscription) {
+      this.cartUpdateSubscription.unsubscribe();
+    }
+  }
+
+  /** Get the current user's id. */
+  getUserId() {
+    const currentUser = this.usersService.getCurrentUser().getValue();
+    if (currentUser) {
+      this.userId = currentUser.userId;
+    } else {
+      console.error('No user logged in!! This shouldn\'t happen');
+    }
+  }
+
+  /** Get the cart entries for the current user. */
+  getCartEntries() {
+    this.cartsService.getCart(this.userId).subscribe({
+      next: (cart) => {
+        if (cart) {
+          const items = Object.entries(cart.productsMap);
+          this.cartEntries = items.map(([sku, quantity]) => new CartMapEntry(Number(sku), quantity));
+          this.cartCount = Number(cart.count);
+          this.totalPrice = Number(cart.totalPrice);
+        } else {
+          console.log('No cart found');
+        }
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
 }
--- estore-ui/src/app/feature/feature.module.ts
+++ estore-ui/src/app/feature/feature.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
+import { RouterModule } from '@angular/router';
 
 import { TestCatalogComponent } from './test-catalog/test-catalog.component';
 import { TestCartComponent } from './test-cart/test-cart.component';
@@ -10,13 +12,16 @@ import { LoginRegistrationComponent } from './login-registration/login-registrat
 import { EditProductComponent } from './edit-product/edit-product.component';
 import { CreateProductComponent } from './create-product/create-product.component';
 import { CartProductCardComponent } from './cart-product-card/cart-product-card.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
-    declarations: [TestCartComponent, TestAdminDashboardComponent, TestCatalogComponent, LoginRegistrationComponent, EditProductComponent, CreateProductComponent, CartProductCardComponent, CartComponent, ShoppingCartComponent],
+    declarations: [TestCartComponent, TestAdminDashboardComponent, TestCatalogComponent, LoginRegistrationComponent, EditProductComponent, CreateProductComponent, CartProductCardComponent, CartComponent],
     imports: [
         CommonModule,
         MaterialModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        AppRoutingModule,
+        RouterModule
     ],
     exports: [
         TestCatalogComponent,
