44e22d7da4e6765b96191a58d08bb560e5036963
Kanisha <kanisha.agrawal@axiomtelecom.com>
2023-03-20 20:52:26 -0400

    Added add to cart button

--- estore-ui/src/app/feature/catalog-product-card/catalog-product-card.component.ts
+++ estore-ui/src/app/feature/catalog-product-card/catalog-product-card.component.ts
@@ -1,17 +1,54 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { Product } from 'src/app/model/product.model';
+import { CartsService } from '../../services/carts.service';
+import { UsersService } from '../../services/users.service';
 @Component({
   selector: 'app-catalog-product-card',
   templateUrl: './catalog-product-card.component.html',
   styleUrls: ['./catalog-product-card.component.css'],
 })
-export class CatalogProductCardComponent {
+export class CatalogProductCardComponent implements OnInit {
   @Input() name = '';
   @Input() price = 0;
   @Input() description = '';
   @Input() image = '';
   @Input() sku = 0;
+  userId: any;
 
-  constructor() {}
+  constructor(
+    private cartsService: CartsService,
+    private usersService: UsersService
+  ) {}
+
+  ngOnInit(): void {
+    this.getUserId();
+  }
+
+  getUserId() {
+    const currentUser = this.usersService.getCurrentUser().getValue();
+
+    if (currentUser) {
+      this.userId = currentUser.userId;
+    } else {
+      console.error("No user logged in!! This shouldn't happen");
+    }
+  }
+
+  // add the selected product to the cart
+  addToCart(sku: number) {
+    console.log(`userId: ${this.userId}, sku: ${sku}`);
+    this.cartsService.addProductToCart(this.userId, sku, 1, true).subscribe({
+      next: (response) => {
+        if (response) {
+          console.log('Product added to cart');
+        } else {
+          console.log('Failed to add product to cart');
+        }
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
