77577ccc96009eec96b460978c75a01125b632f5
Noah Landis <nl7665@rit.edu>
2023-03-20 22:35:40 -0400

    admin dashboard complete

--- estore-ui/src/app/feature/admin-dashboard/admin-dashboard.component.ts
+++ estore-ui/src/app/feature/admin-dashboard/admin-dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { InventoryService } from 'src/app/services/inventory.service';
 import { Product } from 'src/app/model/product.model';
+import { UpdateService } from 'src/app/services/update.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -9,14 +11,24 @@ import { Product } from 'src/app/model/product.model';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
+  private dashboardUpdateSubscription?: Subscription;
   products : Product[] = [];
 
 
-  constructor(private router: Router, private inventoryService: InventoryService) {} 
+  constructor(private router: Router, private inventoryService: InventoryService, private updateService: UpdateService) {} 
+  
   ngOnInit(): void {
     this.getProducts();
+    this.dashboardUpdateSubscription = this.updateService.dashboardUpdate$.subscribe(() => {
+      this.getProducts();
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.dashboardUpdateSubscription) {
+      this.dashboardUpdateSubscription.unsubscribe();
+    }
+  }
 
   getProducts(): void {
     this.inventoryService.getProducts().subscribe(products => { this.products = products });
