aa17d683d0637cb8c19a3d68e70207ec610e0927
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-03-26 22:15:48 -0400

    back merge from sprint3

--- estore-ui/src/app/features/browse-catalog/catalog-product-card/catalog-product-card.component.ts
+++ estore-ui/src/app/features/browse-catalog/catalog-product-card/catalog-product-card.component.ts
@@ -5,13 +5,19 @@ import { CartsService } from 'src/app/core/services/carts.service';
 @Component({
   selector: 'app-catalog-product-card',
   templateUrl: './catalog-product-card.component.html',
-  styleUrls: ['./catalog-product-card.component.css'],
+  styleUrls: ['./catalog-product-card.component.scss'],
 })
+
 export class CatalogProductCardComponent implements OnInit {
   @Input() name = '';
   @Input() price = 0;
   @Input() image = '';
   @Input() sku = 0;
+  @Input() stock = 0;
+  // string representation of the stock to be displayed to customers
+  stockStatus: string = '';
+  private readonly QUANTITY_LOW_STOCK = 10;
+  private readonly QUANTITY_OUT_OF_STOCK = 0; 
   
   constructor(
     private cartsService: CartsService,
@@ -19,6 +25,7 @@ export class CatalogProductCardComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.setStockStatus();
   }
 
   // add the selected product to the cart
@@ -41,5 +48,21 @@ export class CatalogProductCardComponent implements OnInit {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+
+  /**
+   * Updates the string representation of the stock based on the quantity
+   */
+  setStockStatus(): void {
+    if (this.stock == this.QUANTITY_OUT_OF_STOCK) {
+        this.stockStatus = "Out Of Stock";
+    }
+    else if (this.stock <= this.QUANTITY_LOW_STOCK) {
+        this.stockStatus = "Low Stock";
+    }
+    else {
+        this.stockStatus = "In Stock";
+    }
   }
 }
