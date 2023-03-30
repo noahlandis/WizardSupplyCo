ca43a353ba1fb53467b87d12e974b79a1b1d0e77
Ryan <39078738+2nPlusOne@users.noreply.github.com>
2023-03-30 11:54:26 -0400

    Created order model classes

---
+++ estore-ui/src/app/core/model/order.model.ts
@@ -0,0 +1,48 @@
+import { Cart } from "./cart.model";
+
+export class BaseOrder {
+  constructor(
+    public orderDate: Date = new Date(),
+    public firstName: string,
+    public lastName: string,
+    public phoneNumber: string,
+    public emailAddress: string,
+    public shippingAddress: ShippingAddress,
+    public cart: Cart
+  ) { }
+}
+
+export class Order extends BaseOrder {
+  constructor(
+    public orderNumber: number,
+    orderDate: Date,
+    firstName: string,
+    lastName: string,
+    phoneNumber: string,
+    emailAddress: string,
+    shippingAddress: ShippingAddress,
+    cart: Cart
+  ) {
+    super(
+      orderDate,
+      firstName,
+      lastName,
+      phoneNumber,
+      emailAddress,
+      shippingAddress,
+      cart
+    );
+  }
+}
+
+export class ShippingAddress {
+  constructor(
+    public country: string,
+    public state: string,
+    public city: string,
+    public zipCode: string,
+    public addressLine1: string,
+    public addressLine2: string,
+    public apartmentNumber: string,
+  ) { }
+}
