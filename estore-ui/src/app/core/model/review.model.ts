fed5787c6b6ebc0aa99ee0f686c029ac20088a37
pnp2147 <pnp2147@rit.edu>
2023-04-03 23:58:11 -0400

    Modified review class according to suggestions

--- estore-ui/src/app/core/model/review.model.ts
+++ estore-ui/src/app/core/model/review.model.ts
@@ -1,8 +1,20 @@
-export class Review {
+export class BaseReview {
   constructor(
     public userId: number,
     public sku: number,
     public rating: number,
     public comment: string
   ) { }
+}
+
+export class Review extends BaseReview {
+  constructor(
+    public reviewId: number,
+    userId: number,
+    sku: number,
+    rating: number,
+    comment: string
+  ) {
+    super(userId, sku, rating, comment);
+  }
 }
