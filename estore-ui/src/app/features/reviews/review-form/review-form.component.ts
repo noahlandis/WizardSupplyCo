f339054f3cdeec263c8a2da927bbd208d202de2c
pnp2147 <pnp2147@rit.edu>
2023-04-05 17:39:38 -0400

    Added review render component

--- estore-ui/src/app/features/reviews/review-form/review-form.component.ts
+++ estore-ui/src/app/features/reviews/review-form/review-form.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { authAdminGuard } from 'src/app/core/guards/auth-admin.guard';
 import { AuthService } from 'src/app/core/services/auth.service';
-//import { BaseReview,  Review } from 'src/app/core/models/review-model';
+import { BaseReview,  Review } from 'src/app/core/model/review.model';
 import { OrdersService } from 'src/app/core/services/orders.service';
-//import { ReviewService } from 'src/app/core/services/review.service';
+import { ReviewService } from 'src/app/core/services/review.service';
 
 
 @Component({
@@ -17,14 +19,20 @@ export class ReviewFormComponent {
 
   reviewForm : FormGroup;
   stars: number[] = [1, 2, 3, 4, 5];
+  public sku: number = 0;
+  private userId = this.authService.getUserId() || 0;
+  purchasedSkus: number[] = [];
+  private routeSubscription!: Subscription;
+
+
 
   constructor(
     public authService: AuthService,
-    //public authAdminGuard: authAdminGuard,
-    //public reviewService: ReviewService,
+    public reviewService: ReviewService,
     public formBuilder: FormBuilder,
     public ordersService: OrdersService,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute,
 
   ) {
       this.reviewForm = this.formBuilder.group({
@@ -34,16 +42,35 @@ export class ReviewFormComponent {
    }
 
   ngOnInit(): void {    
+    this.routeSubscription = this.route.paramMap.subscribe(params => { 
+      this.sku = Number(params.get('sku'));
+    });
   }
 
-  async create(): Promise<void> {
+  public userHasBoughtProduct(): boolean {
+    let hasBought = false;
+    this.ordersService.getProductsOrderedByUserId(this.userId).subscribe(
+      purchasedSkus => { this.purchasedSkus = purchasedSkus;
+      this.purchasedSkus.forEach(sku => {
+        console.log('sku: ' + sku);
+        if (sku == this.sku) {
+          hasBought = true;
+        }
+      });  
+    });
+    return hasBought;
+  }  
+
+  async create() {
     if(!this.reviewForm.valid)
       return;
-
+    this.userId = this.authService.getUserId() || 0;
+    const rating = this.reviewForm.value.rating;
+    const comment = this.reviewForm.value.comment;
     const review = new BaseReview(
-      this.authService.getUserId(),
-      //sku,
-      this.reviewForm.value.rating,
+      this.userId,
+      this.sku,
+      rating,
       this.reviewForm.value.comment  
     );
     console.log('Create review form submitted with review:', JSON.stringify(review));
--- estore-ui/src/app/features/reviews/reviews.module.ts
+++ estore-ui/src/app/features/reviews/reviews.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../../material.module';
 
-import { MaterialModule } from 'src/app/material.module';
-import { ReviewCardComponent } from './review-card/review-card.component';
-
+import { ReviewFormComponent } from './review-form/review-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ReviewFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    ReviewFormComponent
   ]
 })
 export class ReviewsModule { }
